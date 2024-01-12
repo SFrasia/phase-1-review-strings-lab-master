20cffa95dacf78e2f1c64be5b7ce6cbc9dfb3ad0
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-12 13:12:18 +0300

    Changes

---
+++ index.js
@@ -0,0 +1,9 @@
+// Write your code in this file!
+
+const currentUser = "John Smith";
+console.log(currentUser);
+
+const welcomeMessage = "Welcome to Flatbook";
+console.log(welcomeMessage);
+
+console.log(`${welcomeMessage}, ${currentUser}`);
